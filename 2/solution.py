05e273858778d22a6bbfd7aba9d918f20304e9fd
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2023-12-03 00:01:32 +0100

    2 - part 2

--- 2/solution.py
+++ 2/solution.py
@@ -12,7 +12,6 @@ CubeColor = StrEnum('CubeColor', ['red', 'green', 'blue'])
 # str should be CubeColor
 CubesSet: TypeAlias = dict[str, int]
 
-
 @dataclass
 class Game:
     id: int
@@ -23,6 +22,12 @@ cubes_in_bag = {
     CubeColor.red: 12,
     CubeColor.green: 13,
     CubeColor.blue: 14,
+}
+
+empty_cubes_in_bag = {
+    CubeColor.red: 0,
+    CubeColor.green: 0,
+    CubeColor.blue: 0,
 }
 
 
@@ -65,3 +70,29 @@ def is_possible_game(game: Game) -> bool:
             if count > cubes_in_bag[color]:
                 return False
     return True
+
+
+def part2(input_filename: str):
+    with open(input_filename) as f:
+        games = parse_games(f)
+        return solve2(games)
+
+
+def solve2(games: list[Game]) -> int:
+    minimal_power_of_games = map(find_minimal_power, games)
+    return sum(minimal_power_of_games)
+
+
+def find_minimal_power(game: Game) -> int:
+    minimal_cubes_in_bag = empty_cubes_in_bag
+    for cubes_set in game.revealed_sets:
+        minimal_cubes_in_bag = {
+            CubeColor.red: max(cubes_set.get(CubeColor.red, 0), minimal_cubes_in_bag[CubeColor.red]),
+            CubeColor.green: max(cubes_set.get(CubeColor.green, 0), minimal_cubes_in_bag[CubeColor.green]),
+            CubeColor.blue: max(cubes_set.get(CubeColor.blue, 0), minimal_cubes_in_bag[CubeColor.blue]),
+        }
+
+    power = 1
+    for count in minimal_cubes_in_bag.values():
+        power *= count
+    return power
