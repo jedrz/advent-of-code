745f97f721cb3e343d4d51647ac7ab633221c8a5
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2023-12-05 22:05:52 +0100

    5 - part 2 too long

--- 5/solution.py
+++ 5/solution.py
@@ -31,17 +31,29 @@ class LocationMap:
         return start_location
 
 
-def part1(input_filename: str):
+def part12(input_filename: str):
     with open(input_filename) as f:
         input = f.read()
         splitted_input = input.split('\n\n')
         seeds = parse_seeds(splitted_input[0])
+        seed_ranges = parse_seed_ranges(splitted_input[0])
         location_maps = list(map(parse_map, splitted_input[1:]))
-        solve1(seeds, location_maps)
+        print(solve1(seeds, location_maps))
+        print(solve2(seed_ranges, location_maps))
 
 
 def parse_seeds(line: str) -> list[int]:
     return list(map(int, line.removeprefix('seeds: ').split()))
+
+
+def parse_seed_ranges(line: str) -> list[range]:
+    seeds_str = line.removeprefix('seeds: ')
+    numbers = list(map(int, seeds_str.split()))
+    ranges = []
+    for r in range(0, len(numbers), 2):
+        [start, length] = numbers[r:r + 2]
+        ranges.append(range(start, start + length))
+    return ranges
 
 
 def parse_map(text: str) -> LocationMap:
@@ -56,7 +68,7 @@ def parse_map(text: str) -> LocationMap:
 
 
 def solve1(seeds: list[int], location_maps: list[LocationMap]):
-    print(min(map(lambda seed: find_location(seed, location_maps), seeds)))
+    return min(map(lambda seed: find_location(seed, location_maps), seeds))
 
 
 def find_location(seed: int, location_maps: list[LocationMap]) -> int:
@@ -64,3 +76,9 @@ def find_location(seed: int, location_maps: list[LocationMap]) -> int:
     for location_map in location_maps:
         location = location_map.lookup(location)
     return location
+
+
+def solve2(seed_ranges: list[range], location_maps: list[LocationMap]) -> int:
+    return min(find_location(seed, location_maps)
+               for seed_range in seed_ranges
+               for seed in seed_range)
