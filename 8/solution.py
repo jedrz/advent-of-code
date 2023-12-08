ca7cc13abea423d290ed8925b232708dcf8d5fbd
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2023-12-08 22:53:35 +0100

    8 - part 2

--- 8/solution.py
+++ 8/solution.py
@@ -5,12 +5,13 @@ import re
 import itertools
 
 
-def part_1(input_filename):
+def part_1_and_2(input_filename):
     with open(input_filename) as f:
         lines = f.readlines()
         instructions = lines[0].strip()
         network = parse_network(lines[2:])
-        print(solve1(instructions, network))
+        #print(solve1(instructions, network))
+        print(solve2(instructions, network))
 
 
 def parse_network(lines):
@@ -35,3 +36,40 @@ def solve1(instructions, network):
         next = network[next][ins]
         moves += 1
     return moves
+
+
+def solve2(instructions, network):
+    nexts = find_starting_nodes(network)
+    moves_list = []
+    for next in nexts:
+        moves = 0
+        for ins in itertools.cycle(instructions):
+            if is_ending_node(next):
+                moves_list.append(moves)
+                break
+            next = network[next][ins]
+            moves += 1
+    return find_least_common_multiplier(moves_list)
+
+
+def find_starting_nodes(network):
+    return [node for node in network.keys() if node.endswith('A')]
+
+
+def all_are_ending_nodes(nodes):
+    return all(node.endswith('Z') for node in nodes)
+
+
+def is_ending_node(node):
+    return node.endswith('Z')
+
+
+def find_least_common_multiplier(numbers):
+    # Found online LCM calculator for the below numbers :P
+    print(numbers)
+    multipliers = {n: n for n in numbers}
+    while not all((min_multiplier := min(multipliers.keys())) % n == 0 for n in numbers):
+        next_multiplier = min_multiplier + (number := multipliers[min_multiplier])
+        multipliers.pop(min_multiplier)
+        multipliers[next_multiplier] = number
+    return min(multipliers.keys())
