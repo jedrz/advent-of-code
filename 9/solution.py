191b8907425bdbf1435bad4e606aaa8a475644a9
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2023-12-09 11:40:31 +0100

    9 - part 2

--- 9/solution.py
+++ 9/solution.py
@@ -2,14 +2,18 @@
 # -*- coding: utf-8 -*-
 
 
-def part_1(input_filename):
+def part_1_and_2(input_filename):
     with open(input_filename) as f:
         result = 0
+        result_backwards = 0
         for history_line in f:
             history = list(map(int, history_line.split()))
             next_value = predict_next_value(history)
+            next_value_backwards = predict_next_value_backwards(history)
             result += next_value
+            result_backwards += next_value_backwards
         print(result)
+        print(result_backwards)
 
 
 def predict_next_value(history):
@@ -18,4 +22,12 @@ def predict_next_value(history):
     differences = [j - i for i, j in zip(history[:-1], history[1:])]
     next_value = predict_next_value(differences)
     return history[-1] + next_value
+
+
+def predict_next_value_backwards(history):
+    if not set(history) - {0}:
+        return 0
+    differences = [j - i for i, j in zip(history[:-1], history[1:])]
+    next_value = predict_next_value_backwards(differences)
+    return history[0] - next_value
 
