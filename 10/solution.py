a774bd6f2b826735bf8ca7099825609a83be89c3
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2023-12-10 22:29:52 +0100

    10 - part 1 with classic bfs

--- 10/solution.py
+++ 10/solution.py
@@ -9,7 +9,7 @@ def part_1(input_filename):
     with open(input_filename) as f:
         grid = parse_tiles_grid(f)
         start_pos = find_start_position(grid)
-        return extend_paths(grid, [[start_pos]])
+        return extend_paths(grid, start_pos)
 
 
 def parse_tiles_grid(lines):
@@ -24,24 +24,29 @@ def find_start_position(grid):
             return position
 
 
-def extend_path_once(grid, path):
-    current_pos = path[-1]
-    (x, y) = current_pos
+def extend_paths(grid, start_pos):
+    distances = {start_pos: 0}
+    is_not_visited = lambda pos: pos not in distances
+    current_positions = extend_position(grid, start_pos)
+    for pos in current_positions:
+        distances[pos] = 1
+    while current_positions:
+        current_pos = current_positions[0]
+        current_positions = current_positions[1:]
+        next_positions = list(filter(is_not_visited, extend_position(grid, current_pos)))
+        for next_pos in next_positions:
+            distances[next_pos] = distances[current_pos] + 1
+        current_positions += next_positions
+    return max(distances.values())
+
+
+def extend_position(grid, current_pos):
+    x, y = current_pos
     next_positions = [adj_pos
                       for x_offset, y_offset in [(1, 0), (-1, 0), (0, 1), (0, -1)]
-                      if (adj_pos := (x + x_offset, y + y_offset)) not in path \
-                          and adj_pos in grid \
+                      if (adj_pos := (x + x_offset, y + y_offset)) in grid \
                           and can_go_from(grid[adj_pos], (x_offset, y_offset))]
-    return [path + [next_pos] for next_pos in next_positions]
-
-
-def extend_paths(grid, paths):
-    extended_paths_once = list(itertools.chain(*[extend_path_once(grid, path) for path in paths]))
-    last_positions = [extended_path[-1] for extended_path in extended_paths_once]
-    for last_pos in last_positions:
-        if last_positions.count(last_pos) > 1:
-            return len(extended_paths_once[0]) - 1
-    return extend_paths(grid, extended_paths_once)
+    return next_positions
 
 
 def can_go_from(tile, offset):
