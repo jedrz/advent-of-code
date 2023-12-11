4a6a9b6b0b4554e2f24c84e684c872f5c099c21b
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2023-12-11 21:21:30 +0100

    11 - part 2

--- 11/solution.py
+++ 11/solution.py
@@ -6,28 +6,28 @@ import itertools
 import numpy
 
 
-def part_1(input_filename):
+def part_1_and_2(input_filename):
     with open(input_filename) as f:
         lines_split = [list(line.rstrip()) for line in f]
         universe = numpy.array(lines_split)
-        print(sum_all_galaxies_length(expand(universe)))
+        to_expand = expand(universe)
+        print(sum_all_galaxies_length(universe, 2, to_expand))
+        print(sum_all_galaxies_length(universe, 1_000_000, to_expand))
 
 
 def expand(universe):
     max_x, max_y = universe.shape
     x_indices = [x for x in range(max_x) if set(universe[x]) == {'.'}]
     y_indices = [y for y in range(max_y) if set(universe[:,y]) == {'.'}]
-    x_expanded = numpy.insert(universe, x_indices, '.', axis=0)
-    expanded = numpy.insert(x_expanded, y_indices, '.', axis=1)
-    return expanded
+    return (x_indices, y_indices)
 
 
-def sum_all_galaxies_length(universe):
+def sum_all_galaxies_length(universe, expand_by, to_expand):
     galaxy_positions = [(pos[0].item(), pos[1].item()) for pos in numpy.nditer(numpy.where(universe == '#'))]
     galaxy_pairs = itertools.combinations(galaxy_positions, 2)
     s = 0
     for (g1, g2) in galaxy_pairs:
-        s += manhattan_distance(g1, g2)
+        s += manhattan_distance_expanded(g1, g2, expand_by, to_expand)
     return s
 
 
@@ -35,3 +35,11 @@ def manhattan_distance(p1, p2):
     return sum([abs(c1 - c2) for c1, c2 in zip(p1, p2)])
 
 
+def manhattan_distance_expanded(p1, p2, expand_by, to_expand):
+    return sum([abs(c1 - c2) + (expand_by - 1) * count_between(min(c1, c2), max(c1, c2), to_expand_axis)
+                for c1, c2, to_expand_axis in zip(p1, p2, to_expand)])
+
+
+def count_between(a, b, elems):
+    # kind of range tree could be used
+    return len(list(e for e in elems if a < e < b))
