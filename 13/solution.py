1d66085e888233a900f230e2c07c5b26781dd4ae
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2023-12-13 23:57:10 +0100

    13 - part 1

---
+++ 13/solution.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def part_1(input_filename):
+    with open(input_filename) as f:
+        patterns = [parse_pattern(input_part)
+                    for input_part in f.read().split('\n\n')]
+        print(sum(count_reflection(pattern) for pattern in patterns))
+
+
+def parse_pattern(pattern_s):
+    return pattern_s.split()
+
+
+def count_reflection(pattern):
+    return find_vertical_reflection(pattern) \
+        + 100 * find_horizontal_reflection(pattern)
+
+
+def find_vertical_reflection(pattern):
+    flipped_pattern = ['' for _ in pattern[0]]
+    for line in pattern:
+        for i, c in enumerate(line):
+            flipped_pattern[i] += c
+    return find_horizontal_reflection(flipped_pattern)
+
+
+def find_horizontal_reflection(pattern):
+    for i in range(1, len(pattern)):
+        j = min(i, len(pattern) - i)
+        before = list(reversed(pattern[:i]))[:j]
+        after = pattern[i:i + j]
+        if before == after:
+            return i
+    return 0
+
