ad1297530d1412f2361ca70c5c0b95dd4ec9a622
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2023-12-18 00:04:21 +0100

    17 - part 2 without priority queue too slow

--- 17/solution.py
+++ 17/solution.py
@@ -22,10 +22,11 @@ class PreviousItem:
     direction: tuple[int, int]
 
 
-def part_1(input_filename):
+def part_1_and_2(input_filename):
     with open(input_filename) as f:
         heat_loss_map = parse_heat_loss_map(f)
-        print(find_least_heat_loss(heat_loss_map))
+        print(find_least_heat_loss(heat_loss_map, min_in_same_direction=1, max_in_same_direction=3))
+        print(find_least_heat_loss(heat_loss_map, min_in_same_direction=4, max_in_same_direction=10))
 
 
 def parse_heat_loss_map(lines):
@@ -36,7 +37,7 @@ def parse_heat_loss_map(lines):
     return heat_loss_map
 
 
-def find_least_heat_loss(heat_loss_map):
+def find_least_heat_loss(heat_loss_map, min_in_same_direction, max_in_same_direction):
     start_pos = (0, 0)
     end_pos = max(heat_loss_map.keys())
     distances = defaultdict(lambda: INF)
@@ -49,7 +50,7 @@ def find_least_heat_loss(heat_loss_map):
         if pos == end_pos:
             return distances[(end_pos, direction)]
         q.remove((pos, direction))
-        nexts = get_nexts(heat_loss_map, pos, direction)
+        nexts = get_nexts(heat_loss_map, pos, direction, min_in_same_direction, max_in_same_direction)
         for next_pos, next_dir, next_heat_loss in nexts:
             if distances[(next_pos, next_dir)] > (better_distance := distances[(pos, direction)] + next_heat_loss):
                 distances[(next_pos, next_dir)] = better_distance
@@ -63,11 +64,10 @@ def find_with_shortest_distance(q, distances):
     return min(q, key=distances.get)
 
 
-def get_nexts(heat_loss_map, pos, direction):
+def get_nexts(heat_loss_map, pos, direction, min_in_same_direction, max_in_same_direction):
     directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     # Exclude current direction.
     allowed_directions = list(filter(lambda d: d[0] * direction[0] == d[1] * direction[1] == 0, directions))
-    max_in_same_direction = 3
     possible_nexts = []
     for d in allowed_directions:
         heat_loss = 0
@@ -75,5 +75,6 @@ def get_nexts(heat_loss_map, pos, direction):
             next_pos = (pos[0] + d[0] * times_in_same_dir, pos[1] + d[1] * times_in_same_dir)
             if next_pos in heat_loss_map:
                 heat_loss += heat_loss_map[next_pos]
-                possible_nexts += [(next_pos, d, heat_loss)]
+                if times_in_same_dir >= min_in_same_direction:
+                    possible_nexts += [(next_pos, d, heat_loss)]
     return possible_nexts
